55f2c0aa84236f75b8f0de91f98156d4abb8ce71
Colin Armstrong <colinwarmstrong@gmail.com>
2018-06-18 17:04:52 -0600

    Adds update method to update tasks.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,14 @@
 require 'sqlite3'
 
 class Task
-  attr_reader :title, :description
+  attr_reader :title, :description, :id
 
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
     @database    = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
+    @id = task_params['id'] if task_params["id"]
   end
 
   def save
@@ -15,11 +16,27 @@ class Task
   end
 
   def self.all
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
+    database = Task.database
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
     end
+  end
+
+  def self.find(id)
+    database = Task.database
+    task = database.execute("SELECT * FROM tasks WHERE id = ?", id.to_i).first
+    Task.new(task)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+  def self.update(id, task_params)
+    database.execute("UPDATE tasks SET title = ?, description = ? WHERE id = ?;", task_params[:title], task_params[:description], id)
+    Task.find(id)
   end
 end
