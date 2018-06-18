42ff801940b47a10c2b9c452778550f087687bb0
Colin Armstrong <colinwarmstrong@gmail.com>
2018-06-18 17:14:21 -0600

    Adds delete functionality to our tasks.

--- app/models/task.rb
+++ app/models/task.rb
@@ -39,4 +39,8 @@ class Task
     database.execute("UPDATE tasks SET title = ?, description = ? WHERE id = ?;", task_params[:title], task_params[:description], id)
     Task.find(id)
   end
+
+  def self.destroy(id)
+    database.execute("DELETE FROM tasks WHERE id = ?;", id)
+  end
 end
